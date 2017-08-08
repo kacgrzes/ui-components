160b34fb73bd6d7c4c2af423f689cf101031c267
Kacper Grzeszczyk <kacgrzes@gmail.com>
2017-08-08 17:38:47 +0200

    Modify Label component

--- src/atoms/Label/Label.js
+++ src/atoms/Label/Label.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export const Label = ({children}) => {
   return (
-    <Text>{children}</Text>
+    <Text style={styles.label}>{children}</Text>
   )
 };
+
+const styles = StyleSheet.create({
+  label: {
+    lineHeight: 17,
+    fontSize: 14,
+    textAlign: 'center',
+    fontWeight: '600',
+    color: '#ffffff'
+  }
+})
 
 Label.propTypes = {
 
